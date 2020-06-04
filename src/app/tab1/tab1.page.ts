cc44e49b048a696d11668e80ab4742094ef961c6
Andres <andresreyna15@gmail.com>
2020-06-04 09:01:15 -0500

    Minor v0.5.2 changes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirstoreServiceService } from '../firstore-service.service'
+import { Platillo } from '../models/platillo';
+import { Cache } from '../Cache/cache'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+  public Platillos: Platillo[];
+  public uidCafeteria: string;
 
+  constructor(
+    public servicio: FirstoreServiceService,
+    public router: Router,
+  ) {}
+
+  ngOnInit(){
+    //this.uidCafeteria = Cache.usuario.uid;
+    this.uidCafeteria = "6qEx6po4K9MHafASiJHhXf3NoKr1";
+
+    this.servicio.getPlatillos(this.uidCafeteria).subscribe(platillos => {
+      this.Platillos = platillos;
+
+      console.log(this.Platillos);
+    });
+  }
+
+  abrirPedido(uid: string){
+    console.log('abrirPedido()');
+    console.log(uid);
+
+    this.router.navigate(['/pedido-alumno'],{
+      queryParams:{
+        uidPlatillo: uid,
+      }
+    });
+  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirstoreServiceService } from '../firstore-service.service'
+import { Platillo } from '../models/platillo';
+import { Cache } from '../Cache/cache'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
 
-  constructor() {}
+  public Platillos: Platillo[];
+  public uidCafeteria: string;
 
+  constructor(
+    public servicio: FirstoreServiceService,
+    public router: Router,
+  ) {}
+
+  ngOnInit(){
+    this.uidCafeteria = "iSqw1M3cIhdmWJwOSOYk9wtSt7o1";
+
+    this.servicio.getPlatillos(this.uidCafeteria).subscribe(platillos => {
+      this.Platillos = platillos;
+
+      console.log(this.Platillos);
+    });
+  }
+
+  abrirPedido(uid: string){
+    console.log('abrirPedido()');
+    console.log(uid);
+
+    this.router.navigate(['/pedido-alumno'],{
+      queryParams:{
+        uidPlatillo: uid,
+      }
+    });
+  }
 }
